e77cf29a40add6a1d50d99af7335f4227d243570
shanecranor <shanecranor@gmail.com>
2024-03-26 10:27:16 -0500

    starting to write tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,12 +1,19 @@
+import { BannerAPI } from "..";
+
+beforeEach(() => {
+  (global.fetch as jest.Mock).mockClear();
+  (global.fetch as jest.Mock).mockImplementation(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ data: "mocked data" }), // Mock response data
+    })
+  );
+});
+
 describe("api tests", () => {
-  // it('should return "Hello, World!"', () => {
-  //   expect(helloWorld()).toBe("Hello World");
-  // });
-  // it("should cry ", async () => {
-  //   const response = await fetch(
-  //     "https://nubanner.neu.edu/StudentRegistrationSsb/ssb/term/search?term=202510"
-  //   );
-  //   expect(response.status).toBe(200);
-  //   console.log(response.headers.get("set-cookie"));
-  // });
+  it("should fetch data", async () => {
+    const api = new BannerAPI("http://example.com");
+    const response = await api.bannerFetch("GET", "/endpoint");
+    expect(response).toEqual({ data: "mocked data" });
+  });
 });
